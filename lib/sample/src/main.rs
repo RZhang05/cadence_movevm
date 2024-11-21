b1be65dfc610a1e8058c976c9c8bfafb4de512f9
Raymond Zhang <zhangraymond05@gmail.com>
2024-11-21 10:41:28 -0500

    Updated flamegraph.

--- lib/sample/src/main.rs
+++ lib/sample/src/main.rs
@@ -1,4 +1,14 @@
 mod vm;
 
+use vm::{
+    run_func_M,
+    create_move_composite,
+};
+use std::env;
+
 fn main() {
+    let n = 14;
+    run_func_M(format!("{}/src/build/fib/bytecode_modules/M.mv", env::current_dir().expect("valid cwd").display()), "recur_fib", n);
 }
+
+
